0eb2f8072c1d41d410c20510ab5e089b80c84372
Eduardo Dobay <edudobay@gmail.com>
2017-02-15 00:53:15 -0200

    Add an extra colour component to the page
    Soon we will blend them

--- web/js/main.js
+++ web/js/main.js
@@ -1,10 +1,16 @@
 import {ColorBlock, ColorComponent} from './color-component';
 
 function setupColorComponent() {
-	var display = new ColorBlock(document.getElementById('color-block'));
-	var component = new ColorComponent(document.getElementById('color-input-field'),
-		display);
-	component.bind();
+
+	const entryComponents: Array<HTMLElement> = document.querySelectorAll('.color-entry');
+	entryComponents.forEach(entry => {
+		const input = entry.querySelector('.color-entry__input');
+		const display = entry.querySelector('.color-entry__display');
+
+		const component = new ColorComponent(input, new ColorBlock(display));
+		component.bind();
+	});
+
 }
 
 window.addEventListener('load', function() {
